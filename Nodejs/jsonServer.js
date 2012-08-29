77f50320c993e1357d98476dec0377e7279eb9d6
Michael Vogt <michu@neophob.com>
2012-08-30 00:05:55 +0200

    update json example

--- Nodejs/jsonServer.js
+++ Nodejs/jsonServer.js
@@ -2,8 +2,17 @@ var express = require('express');
 
 var app = express();
 
-app.get('/json2', function(req, res){
+app.get('/json1', function(req, res){
   var json = JSON.stringify({ uno: 1, dos : 2 }, null, '\t');
+  res.send(json);
+});
+
+app.get('/json2', function(req, res){
+     var body = {
+        "sleep" : "20000",
+	"image" : "0xff 0xaa 0xbb"
+      };
+  var json = JSON.stringify(body);
   res.send(json);
 });
 
